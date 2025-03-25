52a4a43bc1a52ca5e9f00f5fcf363a2c13bf1909
kirtimalashetti07 <malashettikirti@gmail.com>
2025-03-25 16:35:35 +0530

    case 2

---
+++ switch_pmg.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    // Displaying the menu options
+    printf("Welcome to the Menu!\n");
+    printf("Please choose an option:\n");
+    printf("1. c 1\n");
+    printf("2. web 2\n");
+    printf("3. data science 3\n");
+    printf("4. java 4\n");
+    printf("5. python 5\n");
+    printf("Enter your choice (1-5): ");
+    
+    // Taking user input
+    scanf("%d", &choice);
+
+    // Responding based on user choice
+    switch (choice) {
+        case 1:
+            printf("You selected Option 1.\n");
+            break;
+        case 2:
+            printf("You selected Option 2.\n");
+            break;
+       
+ 
